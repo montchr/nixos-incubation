0db7650c859230df28001ed9129eba4b8c3329b6
Chris Montgomery <chris@cdom.io>
2021-09-24 03:12:08 -0400

    tweak: re-enable disabled system features

--- profiles/core/default.nix
+++ profiles/core/default.nix
@@ -4,7 +4,7 @@ in
 {
   imports = [ ../cachix ];
 
-#  nix.systemFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+  nix.systemFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
 
   environment = {
 
